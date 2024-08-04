1fee5a1d984d4a4c39f701f1432a6e962f4e7acd
pratik4955 <98948130+pratik4955@users.noreply.github.com>
2024-08-04 15:31:32 +0530

    adding user profile form

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -4,6 +4,7 @@ import HomePage from "./pages/HomePage";
 import AuthCallBackPage from "./pages/AuthCallBackPage";
 import UserProfilePage from "./pages/UserProfilePage";
 import ProtectedRoute from "./auth/ProtectedRoute";
+import ManageRestaurantPage from "./pages/ManageRestaurantPage";
 
 const AppRoutes=()=>{
     return(
@@ -11,6 +12,7 @@ const AppRoutes=()=>{
             <Route path="/" element={<Layout showHero><HomePage/></Layout>}/>
             <Route path="/auth-callback" element={<AuthCallBackPage/>}/>
             <Route element={<ProtectedRoute/>}><Route path="/user-profile" element={<Layout ><UserProfilePage/></Layout>}/></Route>
+            <Route path="/manage-restaurant" element={<Layout ><ManageRestaurantPage/></Layout>}/>
             <Route path="*" element={<Navigate to="/"/>}/>
         </Routes>
     );
